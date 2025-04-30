bf16267271f8b0192222caeed275b0bbff4c8a8d
namwook <tomato_zam@naver.com>
2025-04-30 16:21:23 +0900

    Gop

--- CalculatorCodeReview/main.cpp
+++ CalculatorCodeReview/main.cpp
@@ -3,12 +3,25 @@
 class Cal {
 public:
     // 이곳에 코드 작성
+    int getGop(int a, int b)
+    {
+        return a * b;
+    }
+
 };
 
 // 테스트 케이스 작성
 TEST(t1, t2) {
     EXPECT_EQ(1, 1);
 }
+
+TEST(t1, GopTestsuite) {
+    Cal test;
+    int expectedValue = 100;
+    int resultValue = test.getGop(10, 10);
+    EXPECT_EQ(expectedValue, resultValue);
+}
+
 
 int main() {
     ::testing::InitGoogleMock();
